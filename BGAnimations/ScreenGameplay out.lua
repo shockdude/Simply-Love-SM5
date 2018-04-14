c7ef2ee01dacf1998344a8a5160b2a777f4c5a14
shockdude <shockdudex@gmail.com>
2018-04-14 14:42:51 -0700

    Decrease gameplay fadeout time by 1 second

--- BGAnimations/ScreenGameplay out.lua
+++ BGAnimations/ScreenGameplay out.lua
@@ -1,4 +1,4 @@
 return Def.Quad{
 	InitCommand=function(self) self:FullScreen():diffuse(0,0,0,0) end,
-	OnCommand=function(self) self:sleep(1):linear(3):diffusealpha(1) end
+	OnCommand=function(self) self:sleep(1):linear(2):diffusealpha(1) end
 }
